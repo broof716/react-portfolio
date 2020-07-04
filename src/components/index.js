707a785d7caf85cc7fd8c7c26db85671a082a019
Bryan Roof <broof716@gmail.com>
2020-07-04 15:13:59 -0700

    added circles to react-particles

--- src/components/index.js
+++ src/components/index.js
@@ -40,9 +40,19 @@ const Home = () => {
               random: true,
               anim: {
                 enable: true,
-                speed: 10,
+                speed: 6,
                 size_min: 0.1,
-                sync: false
+                sync: true
+              }
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: true
               }
             }
           }
