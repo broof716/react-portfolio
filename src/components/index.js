4f0a2f34edb777f8eb407b0c7a3062f574ca13bb
Bryan Roof <broof716@gmail.com>
2020-07-04 15:09:35 -0700

    added circles to react-particles

--- src/components/index.js
+++ src/components/index.js
@@ -22,7 +22,28 @@ const Home = () => {
         params={{
           particles: {
             number: {
-              value: 45
+              value: 45,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 1,
+                color: "tomato"
+              }
+            },
+            size:{
+              value: 8,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 10,
+                size_min: 0.1,
+                sync: false
+              }
             }
           }
         }}
