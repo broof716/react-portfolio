4e4c649e1cda42b4ebe0c66e173e554c23ccc78c
Bryan Roof <broof716@gmail.com>
2020-07-04 12:29:16 -0700

    added react-particles-js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,18 +10,36 @@ const useStyles = makeStyles(theme=>({
     width: theme.spacing(15),
     height: theme.spacing(15),
     margin: theme.spacing(1)
+  },
+  title: {
+    color: "tomato"
+  },
+  subtitle: {
+    color: "tan",
+    marginBottom: "3rem"
+  },
+  typedContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "100vw",
+    textAlign: "center",
+    zIndex: 1
   }
 }));
 const Header = () => {
   const classes = useStyles()
   return (
-    <Box>
-      <Avatar className={classes.avatar} src={avatar} alt="Bryan Roof"/> 
-      <Typography variant="h4">
+    <Box className={classes.typedContainer}>
+      <Grid container justify="center">
+        <Avatar className={classes.avatar} src={avatar} alt="Bryan Roof"/> 
+      </Grid>
+      <Typography className={classes.title} variant="h4">
         <Typed strings={["Bryan Roof"]} typeSpeed={40} />
       </Typography>
       <br/>
-      <Typography variant="h5">
+      <Typography className={classes.subtitle} variant="h5">
         <Typed strings={["Web Design", "Web Development", "React Developer"]} typeSpeed={40} backSpeed={60} loop />
       </Typography>   
     </Box>
